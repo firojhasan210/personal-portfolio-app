efcfde23f9ea24e078e8d5d5f79eea906cfdd1b1
firojhasan210 <108195295+firojhasan210@users.noreply.github.com>
2023-08-07 22:41:33 +0530

    css update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,39 +15,49 @@ const Footer = () => {
       <div className="footer-container">
         <div className="left">
             <div className="location">
-                <FaHome size={20} style={{color :
+               
+               <div>
+               <FaHome size={20} style={{color :
+                "#fff", marginRight: "1rem"}}/>
+                <p>Kolkata 700009  </p>
+                <p>India.</p>
+               </div>
+               </div>
+                
+               <div>
+               <h4> <FaPhone size={20} style={{color :
                 "#fff", marginRight: "2rem"}}/>
-            <p>Kolkata 700009  </p>
-            <p>India.</p>
-            </div>
-            <div>
-            <h4> <FaPhone size={20} style={{color :
-            "#fff", marginRight: "2rem"}}/>
-            <p>9593422553</p></h4>
-            </div>
-                <h4> <FaMailBulk size={20} style={{color :
-            "#fff", marginRight: "2rem"}}/>
-            <p>firojhassan19990@gmail.com</p></h4>
-    
+                <p>9593422553</p></h4>
+               </div>
+                    <h4> <FaMailBulk size={20} style={{color :
+                "#fff", marginRight: "2rem"}}/>
+                <p>firojhassan19990@gmail.com</p></h4>
+        
+           
            </div>
-        </div>
-        < div className="right">
-            <h4>
-                About My Self
-            </h4>
-            <p>This is FiI have complete my
-                 masters in Computer Application at
-                Calcutta University, Kolkata in 2023.</p>
+        
+        <div  className="right">
+                <h4>
+                    About My Self
+                </h4>
+                <p>This is Firoj.I have complete my
+                    masters in Computer Application at
+                    Calcutta University, Kolkata in 2023.</p>
             <div className="social">
-                <h4> <FaFacebook size={30} style={{color :
-            "#fff", marginRight: "1rem"}}/>
-                </h4>
-                <h4> <FaLinkedin size={30} style={{color :
-            "#fff", marginRight: "1rem"}}/>
-                </h4>
-                <h4> <FaTwitter size={30} style={{color :
-            "#fff", marginRight: "1rem"}}/>
-                </h4>
+               
+                  <h4> <FaFacebook size={30} style={{color :
+                "#fff", marginRight: "1rem"}}/>
+                    </h4>
+                   
+                    <h4> <FaLinkedin size={30} style={{color :
+                "#fff", marginRight: "1rem"}}/>
+                    </h4>
+                    
+                    <h4> <FaTwitter size={30} style={{color :
+                "#fff", marginRight: "1rem"}}/>
+                    </h4>
+                  
+            </div>
             </div>
         
     </div>
